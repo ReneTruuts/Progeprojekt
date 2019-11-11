ce64ed4badeb592377f27afbbb7e6beffdf8a8dd
ReneTruuts <56917905+ReneTruuts@users.noreply.github.com>
2019-11-11 11:32:20 +0200

    Update3
    Kirjutasin lõunaseid ja õhtuseid tegevusi, samuti on lõunase juures valmis ajakulu.

--- w0k3.py
+++ w0k3.py
@@ -1,15 +1,25 @@
 g = input("Kas praegu on hommik, päev või õhtu: ")
 kell = input("Sisesta kellaaeg, mil sa kuskil olema pead: ")
 if g.lower() == "hommik":
-    a = input("Palju aega (minutites) planeerid sa kulutada hommikusöögile?")
-    b = 
+    a = input("Palju aega (minutites) planeerid sa kulutada pesemisele?")
+    b = input("Palju aega (minutites) planeerid sa kulutada hommikusöögile?")
     
     
     
     
     
 if g.lower() == "päev":
-    a = input("Palju aega (minutites) planeerid sa kulutada lõunasöögile?")
+    a = int(input("Palju aega (minutites) planeerid sa kulutada lõunasöögile?"))
+    b = int(input("Palju aega (minutites) planeerid sa kulutada sarja vaatamisele?"))
+    c = int(input("Palju aega (minutites) planeerid sa kulutada koristamisele?"))
+    d = int(input("Palju aega (minutites) planeerid sa kulutada enda korrastamisele?"))
+    e = int(input("Palju aega (minutites) kulutad sa koti kokku pakkimisele?"))
+    f = int(input("Palju aega (minutites) kulutad sa kohalejõudmisele?"))
+    summa = a + b + c + e + d + f
+    tunnid = summa // 60
+    minutid = summa - (tunnid * 60)
+    print("Sinu tegevusteks läheb kokku " + str(tunnid) + " tundi " + "ja " + str(minutid) + " minutit.")
+    
 
 
 
@@ -20,6 +30,7 @@ if g.lower() == "õhtu":
     c = input("Palju aega (minutites) planeerid sa kulutada meigile?")
     d = input("Palju aega (minutites) planeerid sa kulutada riietumisele?")
     e = input("Palju aega (minutites) planeerid sa kulutada soojendamisele?")
+    f = input("Palju aega (minutites) kulutad sa kohalejõudmisele?")
     
     
     
