f80184b7f873c3fe48d583aee38c3bfa26347b73
ReneTruuts <56917905+ReneTruuts@users.noreply.github.com>
2019-11-11 11:14:36 +0200

    Ma tegin mingisuguse muudatuse juba meie programmi suunas.
    Lisasin mingeid tegevusi

--- w0k3.py
+++ w0k3.py
@@ -1,11 +1,25 @@
-import easygui
-easygui.msgbox("See ongi graafiline liides")
+g = input("Kas praegu on hommik, päev või õhtu: ")
+kell = input("Sisesta kellaaeg, mil sa kuskil olema pead: ")
+if g.lower() == "hommik":
+    a = input("Palju aega (minutites) planeerid sa kulutada hommikusöögile?")
+    b = 
+    
+    
+    
+    
+    
+if g.lower() == "päev":
+    a = input("Palju aega (minutites) planeerid sa kulutada lõunasöögile?")
 
 
 
-#print("Hello world")
-#print("Olen usin")
-#from turtle import *
-#for i in range(4):
- #   forward(100)
-  #  right(90)
+
+if g.lower() == "õhtu":
+    a = input("Palju aega (minutites) planeerid sa kulutada pesemisele?")
+    b = input("Palju aega (minutites) planeerid sa kulutada õhtusöögile?")
+    c = input("Palju aega (minutites) planeerid sa kulutada meigile?")
+    d = input("Palju aega (minutites) planeerid sa kulutada riietumisele?")
+    e = input("Palju aega (minutites) planeerid sa kulutada soojendamisele?")
+    
+    
+    
